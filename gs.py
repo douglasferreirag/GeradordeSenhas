79b4dfdc33f7d25c8563dd3a083593505b51a1c9
douglasferreirag <douglas.ferreirag@gmail.com>
2021-06-25 23:36:09 -0300

    primeiro commit

---
+++ gs.py
@@ -0,0 +1,7 @@
+import random, string
+
+tamanho = int(input('Digite o tamanho de senha que você deseja: '))
+chars = string.ascii_letters + string.digits + '!@-=,.;:/?'
+rnd = random.SystemRandom()
+print(''.join(rnd.choice(chars) for i in range(tamanho) ))
+
